76393cec17ec67cc1adddf47f0752e8b1c4feba9
BELAY-hun <belayhunarage.24@gmail.com>
2020-01-05 02:15:07 +0300

    creating idea repository

---
+++ User/Repository/IdeaRepository.go
@@ -0,0 +1,15 @@
+package repository
+
+/*This package Will Responsibel For Manipulating the database and Genereating an Instance of User to be used by the Service */
+import (
+	//entity "github.com/BELAY-hun/Projects/Inovide/models"
+	"github.com/jinzhu/gorm"
+)
+
+type IdeaRepo struct {
+	db *gorm.DB
+}
+
+func NewIdeaRepo(sqlite *gorm.DB) *IdeaRepo {
+	return &IdeaRepo{db: sqlite}
+}
